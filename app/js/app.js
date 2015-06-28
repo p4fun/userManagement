62567ff5e82aeb68baa5e625250672d8ae71e085
Palash Mukhopadhyay <palash.siue@gmail.com>
2015-06-29 04:25:33 +0530

    Adding changes for required functionality

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('UserManagementApp', [
-  'UserManagementApp.controllers','ngRoute'
+  'UserManagementApp.controllers','ngRoute','ngSanitize'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-	when("/login", {templateUrl: "partials/login.html", controller: "driversController"}).
-	when("/register", {templateUrl: "partials/reg.html", controller: "driversController"}).
+	when("/login", {templateUrl: "partials/login.html", controller: "loginController"}).
+	when("/register", {templateUrl: "partials/reg.html", controller: "loginController"}).
+        when("/landing", {templateUrl: "partials/landing.html", controller: "loginController"}).
 	otherwise({redirectTo: '/login'});
 }]);
--- userManagement.py
+++ userManagement.py
@@ -3,7 +3,7 @@ import uuid
 import cgi
 import json
 import urllib2
-import os
+from google.appengine.api import users
 from google.appengine.ext import ndb
 
 class User(ndb.Model):
@@ -36,7 +36,6 @@ class MessageHandler(webapp2.RequestHandler):
 	   data['fromUser'] = message.fromUser.get().id
            data['toUser'] = message.toUser.get().id
            data['content'] = message.content
-           """data['date'] = message.date"""
            j += 1
            if j != i:
            	messageData += json.dumps(data) + ","
@@ -52,11 +51,9 @@ class MessageHandler(webapp2.RequestHandler):
 	toUserIdParam = jsonobject.get('toId')
 	contentParam = jsonobject.get('message')
         fromUserKey = self.getUser(fromUserIdParam).key
-        print ">>>fromUserKey="+str(fromUserKey)
 	toUserKey = self.getUser(toUserIdParam).key
         message = Message(id=str(uuid.uuid1()),content=contentParam,fromUser=fromUserKey,toUser= toUserKey)
         newMessageKey = message.put()
-        print ">>>newMessageKey=" + str(newMessageKey)
         toUser = toUserKey.get()
         toUser.messages.append(newMessageKey)
         toUser.put()
@@ -69,6 +66,23 @@ class MessageHandler(webapp2.RequestHandler):
 
     def delete(self):
 	ndb.delete_multi(Message.query().fetch(keys_only=True)) 
+
+
+class UserHTMLMsgHandler(webapp2.RequestHandler):
+    def post(self):
+	jsonstring = self.request.body
+	jsonobject = json.loads(jsonstring)
+	userId = jsonobject.get('id')
+	users = User.query().order(User.id)
+        messageData = ''
+        for user in users:
+          if str(user.id)==userId:
+	    i = len(user.messages)
+            j = 0
+            for message in user.messages:
+	      messageEntity = message.get()
+	      messageData = 'id='+cgi.escape(messageEntity.id)+'|fromUser='+cgi.escape(messageEntity.fromUser.get().id)+"|content="+cgi.escape(messageEntity.content)     
+      	      self.response.out.write('<blockquote>%s</blockquote>' % messageData)
 """
 API to get user by id
 """
@@ -95,7 +109,6 @@ class UserHandler(webapp2.RequestHandler):
             print "i>>" + str(i)
             j = 0
             for message in user.messages:
-		print ">>for-get"+ str(message)
                 messageEntity = message.get()
 		msg['fromUser'] = messageEntity.fromUser.get().id
 		msg['content'] = messageEntity.content
@@ -109,6 +122,39 @@ class UserHandler(webapp2.RequestHandler):
             userData = json.dumps(data)
 	self.response.headers['Content-Type'] = 'application/json'
 	self.response.out.write(userData)
+
+class LoginHandler(webapp2.RequestHandler):
+    def post(self):
+        jsonstring = self.request.body
+	jsonobject = json.loads(jsonstring)
+	email = jsonobject.get('email')
+        password = jsonobject.get('pass')
+        data = {}
+        user = self.findUserByEmailAndPassword(email,password)
+        if user!=None:
+	  data['id'] = user.id
+	  data['name'] = user.firstName + " " + user.middleName + " " + user.lastName
+	  data['email'] = email
+	else:
+	  data['id'] = data['name'] = data['email'] = None
+	self.response.headers['Content-Type'] = 'application/json'
+	self.response.out.write(json.dumps(data))
+
+    def findUserByEmailAndPassword(self,emailParam,passwordParam):
+	user = User.query(User.email == emailParam,User.password == passwordParam)
+	if user!=None:
+           return user.get()
+        else:
+           return None
+
+class UserRegHandler(webapp2.RequestHandler):
+     def get(self):
+        users = User.query()
+        for user in users:
+           userData = 'id='+cgi.escape(user.id)+'|fname='+cgi.escape(user.firstName)+'|mname='+cgi.escape(user.middleName)
+	   userData += '|lname='+cgi.escape(user.lastName)+'|email='+cgi.escape(user.email)+'|password='+cgi.escape(user.password)
+      	   self.response.out.write('<blockquote>%s</blockquote>' % userData)
+
 
 class UserRegistrationHandler(webapp2.RequestHandler):
     def post(self):
@@ -139,7 +185,7 @@ class UserRegistrationHandler(webapp2.RequestHandler):
            data['lname'] = user.lastName
            data['email'] = user.email
            data['password'] = user.password
-           data['messages'] = user.messages
+           """data['messages'] = user.messages"""
            j += 1
            if j != i:
            	userData += json.dumps(data) + ","
@@ -153,8 +199,11 @@ class UserRegistrationHandler(webapp2.RequestHandler):
 
 
 app = webapp2.WSGIApplication([
+    (r'/auth' , LoginHandler),
     (r'/users', UserRegistrationHandler),
     (r'/user', UserHandler),
     (r'/deleteUsers',UserRegistrationHandler),
     (r'/sendMessage',MessageHandler),
+    (r'/usersHtml', UserRegHandler),
+    (r'/messageHtml',UserHTMLMsgHandler),
 ])
